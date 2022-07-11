878defaa6b0339b33fce994aafbd5ed94f1aa532
Rafael Vigo <rvigo07+github@gmail.com>
2022-07-10 22:49:43 -0300

    chore: adds sha256 values

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -1,19 +1,20 @@
 class Hello < Formula
     desc "hello app"
     homepage "https://github.com/rvigo/hello"
-    version "1.5.1"
+    version "1.5.0"
   
     on_macos do
       if Hardware::CPU.arm?
         url "https://github.com/rvigo/hello/releases/download/v1.5.0/hello-aarch64-apple-darwin.zip"
-    
+        sha256 "9dc7d7a457845b64e4546bf5450a72840deb4845b8b8ad22f1990c54309ffe58"
         def install
           bin.install "hello"
         end
       end 
       if Hardware::CPU.intel?
         url "https://github.com/rvigo/hello/releases/download/v1.5.0/hello-x86_64-apple-darwin.zip"
-    
+        sha256 "9dc7d7a457845b64e4546bf5450a72840deb4845b8b8ad22f1990c54309ffe58"
+
         def install
           bin.install "hello"
         end
@@ -23,14 +24,16 @@ class Hello < Formula
     on_linux do
       if Hardware::CPU.arm?
         url "https://github.com/rvigo/hello/releases/download/v1.5.0/hello-aarch64-unknown-linux-gnu.tar.gz"
-    
+        sha256 "9dc7d7a457845b64e4546bf5450a72840deb4845b8b8ad22f1990c54309ffe58"
+
         def install
           bin.install "hello"
         end
       end 
       if Hardware::CPU.intel?
         url "https://github.com/rvigo/hello/releases/download/v1.5.0/hello-x86_64-unknown-linux-gnu.tar.gz"
-    
+        sha256 "9dc7d7a457845b64e4546bf5450a72840deb4845b8b8ad22f1990c54309ffe58"
+
         def install
           bin.install "hello"
         end
