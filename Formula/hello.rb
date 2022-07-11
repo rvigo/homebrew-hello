38211ac9502a3148f415b6e8fe0df0e0330e73aa
Rafael Vigo <rvigo07+github@gmail.com>
2022-07-10 22:22:22 -0300

    feat: adjusts formula to identify system archs

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -1,10 +1,39 @@
 class Hello < Formula
     desc "hello app"
     homepage "https://github.com/rvigo/hello"
-    url "https://github.com/rvigo/hello/releases/download/v1.5.0/hello-aarch64-apple-darwin.zip"
     version "1.5.1"
   
-    def install
-      bin.install "hello"
+    on_macos do
+      if Hardware::CPU.arm?
+        url "https://github.com/rvigo/hello/releases/download/v1.5.0/hello-aarch64-apple-darwin.zip"
+    
+        def install
+          bin.install "hello"
+        end
+      end 
+      if Hardware::CPU.intel?
+        url "https://github.com/rvigo/hello/releases/download/v1.5.0/hello-x86_64-apple-darwin.zip"
+    
+        def install
+          bin.install "hello"
+        end
+      end 
+    end
+
+    on_linux do
+      if Hardware::CPU.arm?
+        url "https://github.com/rvigo/hello/releases/download/v1.5.0/hello-aarch64-unknown-linux-gnu.tar.gz"
+    
+        def install
+          bin.install "hello"
+        end
+      end 
+      if Hardware::CPU.intel?
+        url "https://github.com/rvigo/hello/releases/download/v1.5.0/hello-x86_64-unknown-linux-gnu.tar.gz"
+    
+        def install
+          bin.install "hello"
+        end
+      end 
     end
   end
