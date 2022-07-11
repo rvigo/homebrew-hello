1501cbbb8a0b7dd796a8e0ddf397cf84966a928e
Rafael Vigo <rvigo@users.noreply.github.com>
2022-07-11 15:37:24 +0000

    hello 1.6.1

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -1,19 +1,19 @@
 class Hello < Formula
     desc "hello app"
     homepage "https://github.com/rvigo/hello"
-    version "1.6.0"
+    version "1.6.1"
   
     on_macos do
       if Hardware::CPU.arm?
         url "https://github.com/rvigo/hello/releases/download/v1.5.0/hello-aarch64-apple-darwin.zip"
-        sha256 "9dcb42d30ef58f37a7bc4bb860392d32fa6c4ed155c0ca0ab1665b5f81c44ced"
+        sha256 "3e3bf3908e5330afbc59bc6094a94e64f0751db38a13b5017bcfe4e982061612"
         def install
           bin.install "hello"
         end
       end 
       if Hardware::CPU.intel?
         url "https://github.com/rvigo/hello/releases/download/v1.5.0/hello-x86_64-apple-darwin.zip"
-        sha256 "9dcb42d30ef58f37a7bc4bb860392d32fa6c4ed155c0ca0ab1665b5f81c44ced"
+        sha256 "3e3bf3908e5330afbc59bc6094a94e64f0751db38a13b5017bcfe4e982061612"
 
         def install
           bin.install "hello"
@@ -24,15 +24,15 @@ class Hello < Formula
     on_linux do
       if Hardware::CPU.arm?
         url "https://github.com/rvigo/hello/releases/download/v1.5.0/hello-aarch64-unknown-linux-gnu.tar.gz"
-        sha256 "9dcb42d30ef58f37a7bc4bb860392d32fa6c4ed155c0ca0ab1665b5f81c44ced"
+        sha256 "3e3bf3908e5330afbc59bc6094a94e64f0751db38a13b5017bcfe4e982061612"
 
         def install
           bin.install "hello"
         end
       end 
       if Hardware::CPU.intel?
-        url "https://github.com/rvigo/hello/releases/download/v1.6.0/hello-x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "9dcb42d30ef58f37a7bc4bb860392d32fa6c4ed155c0ca0ab1665b5f81c44ced"
+        url "https://github.com/rvigo/hello/releases/download/v1.6.1/hello-x86_64-unknown-linux-gnu.tar.gz"
+        sha256 "3e3bf3908e5330afbc59bc6094a94e64f0751db38a13b5017bcfe4e982061612"
 
         def install
           bin.install "hello"
